8029af6676da1ec2216449e9fda0d19a58997ce1
raneem152 <raneemabdelwahab15@gmail.com>
2022-09-09 00:26:08 +0200

    third-ver

---
+++ 1st basic calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    double num1,num2;
+    printf("enter the first number :");
+    scanf("%lf",&num1);
+    printf("enter the second number :");
+    scanf("%lf",&num2);
+    
+    printf("the sum is %f",num1+num2);
+    return 0;
+}
+  
